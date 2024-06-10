42e3af8ec2a74c520c396d508246eccf784e2b23
essodinho45@gmail.com <ALKHAZENSOFT\isam>
2024-06-10 22:18:49 +0300

    fixes

--- resources/views/welcome2.blade.php
+++ resources/views/welcome2.blade.php
@@ -57,37 +57,40 @@
 <body>
 
     <header class="site-header sticky-top py-1">
-        <nav class="container d-flex flex-column flex-md-row justify-content-between">
-            <a class="py-2" href="https://getbootstrap.com/docs/5.0/examples/product/#" aria-label="Product">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
-                    stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    class="d-block mx-auto" role="img" viewBox="0 0 24 24">
-                    <title>Product</title>
-                    <circle cx="12" cy="12" r="10"></circle>
-                    <path
-                        d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94">
-                    </path>
-                </svg>
-            </a>
-            <a class="py-2 d-none d-md-inline-block"
-                href="https://getbootstrap.com/docs/5.0/examples/product/#">Tour</a>
-            <a class="py-2 d-none d-md-inline-block"
-                href="https://getbootstrap.com/docs/5.0/examples/product/#">Product</a>
-            <a class="py-2 d-none d-md-inline-block"
-                href="https://getbootstrap.com/docs/5.0/examples/product/#">Features</a>
-            <a class="py-2 d-none d-md-inline-block"
-                href="https://getbootstrap.com/docs/5.0/examples/product/#">Enterprise</a>
-            <a class="py-2 d-none d-md-inline-block"
-                href="https://getbootstrap.com/docs/5.0/examples/product/#">Support</a>
-            <a class="py-2 d-none d-md-inline-block"
-                href="https://getbootstrap.com/docs/5.0/examples/product/#">Pricing</a>
-            <a class="py-2 d-none d-md-inline-block"
-                href="https://getbootstrap.com/docs/5.0/examples/product/#">Cart</a>
+        <nav class="navbar navbar-expand-md navbar-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand d-md-none" href="#">
+                    <img src="/images/logow.png" alt="">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Features</a>
+                        </li>
+                        <a class="navbar-brand d-none d-md-block" href="#">
+                            <img src="/images/logow.png" alt="">
+                        </a>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
     </header>
 
     <main>
-        <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+        <div class="position-relative overflow-hidden p-3 p-md-5 text-center bg-light">
             <div class="col-md-5 p-lg-5 mx-auto my-5">
                 <h1 class="display-4 fw-normal">Punny headline</h1>
                 <p class="lead fw-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with
