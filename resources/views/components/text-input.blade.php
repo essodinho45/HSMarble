6e68536bfa6a8e88f550c2bdce3aaf8668ba920c
essodinho45@gmail.com <ALKHAZENSOFT\isam>
2024-06-17 15:52:17 +0300

    fix update profile

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,6 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
+    'class' =>
+        'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm read-only:bg-gray-100 read-only:cursor-not-allowed read-only:focus:border-gray-300 read-only:focus:ring-0',
+]) !!}>
